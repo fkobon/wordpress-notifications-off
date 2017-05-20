ee88f9f995543148264360e0e47ee210e35db0ba
African Hope <samuelguebo@gmail.com>
2017-05-20 13:37:35 +0000

    configuring notifications

--- class.notifications-off-menu.php
+++ class.notifications-off-menu.php
@@ -7,10 +7,19 @@ Class NotificationsOffMenu{
 	}
 
 	public function create_menu() {
-		add_menu_page('Notifications Off Plugin','Notifications Off','administrator', 'menu-notifications-off-home','','dashicons-clipboard');
+		add_menu_page( 'Notifications Off Plugin',
+			'Notifications Off','administrator', 
+			'menu-notifications-off-home',
+			'',
+			plugin_dir_url( __FILE__ ) .'views/img/menu-icon.png'
+		);
 
-		add_submenu_page( 'menu-notifications-off-home', 'Settings', 'Notifications status settings',
-			'manage_options', 'menu-notifications-off-home', array( $this, "menu_notifications_off_home"));
+		add_submenu_page( 'menu-notifications-off-home', 
+			'Settings', 'Notifications status settings',
+			'manage_options', 
+			'menu-notifications-off-home', 
+			array( $this, "menu_notifications_off_home")
+		);
 	}
 
 	public function menu_notifications_off_home() {
